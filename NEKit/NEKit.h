cebaf7209c69665c75f2dc37601874641da3d695
Zhuhao Wang <zhuhaow@gmail.com>
2016-05-06 21:38:53 +0800

    Initial Commit

---
+++ NEKit/NEKit.h
@@ -0,0 +1,19 @@
+//
+//  NEKit.h
+//  NEKit
+//
+//  Created by Zhuhao Wang on 5/6/16.
+//  Copyright © 2016 Zhuhao Wang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NEKit.
+FOUNDATION_EXPORT double NEKitVersionNumber;
+
+//! Project version string for NEKit.
+FOUNDATION_EXPORT const unsigned char NEKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NEKit/PublicHeader.h>
+
+
