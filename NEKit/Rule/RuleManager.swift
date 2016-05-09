0e9be41f1fbbacc1a3dc725aa38369aa3cd94343
Zhuhao Wang <zhuhaow@gmail.com>
2016-05-09 13:12:12 +0800

    Add some debug info when matching rules

--- NEKit/Rule/RuleManager.swift
+++ NEKit/Rule/RuleManager.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CocoaLumberjackSwift
 
 public class RuleManager {
     public static var currentManager: RuleManager = RuleManager(fromRules: [], appendDirect: true)
@@ -16,7 +17,10 @@ public class RuleManager {
     func match(request :ConnectRequest) -> AdapterFactoryProtocol! {
         for rule in rules {
             if let adapterFactory = rule.match(request) {
+                DDLogVerbose("Rule \(rule) matches request: \(request)")
                 return adapterFactory
+            } else {
+                DDLogVerbose("Rule \(rule) does not match request: \(request)")
             }
         }
         return nil // this should never happens
