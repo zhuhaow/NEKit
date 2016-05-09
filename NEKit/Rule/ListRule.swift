585c204b0cc2fdf122fd8ac10a813a2f7c5d16ba
Zhuhao Wang <zhuhaow@gmail.com>
2016-05-09 13:13:22 +0800

    Add list-based rule support
    The implementation is very alpha which can and will be improved later.

---
+++ NEKit/Rule/ListRule.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+class ListRule: Rule {
+    var urls: [NSRegularExpression] = []
+    let adapterFactory: AdapterFactoryProtocol
+    
+    init(adapterFactory: AdapterFactoryProtocol, urls: [String]) throws {
+        self.adapterFactory = adapterFactory
+        self.urls = try urls.map {
+            try NSRegularExpression(pattern: $0, options: .CaseInsensitive)
+        }
+    }
+    
+    override func match(request: ConnectRequest) -> AdapterFactoryProtocol? {
+        for url in urls {
+            if let _ = url.firstMatchInString(request.host, options: [], range: NSRange(location: 0, length: request.host.utf16.count)) {
+                return adapterFactory
+            }
+        }
+        return nil
+    }
+}
