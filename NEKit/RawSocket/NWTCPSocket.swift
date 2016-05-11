dc3d91fe5f54bd2c8c530e587bb7ec3f1cd78ee7
Zhuhao Wang <zhuhaow@gmail.com>
2016-05-11 20:14:49 +0800

    Implement readDataToData in NWTCPSocket

--- NEKit/RawSocket/NWTCPSocket.swift
+++ NEKit/RawSocket/NWTCPSocket.swift
@@ -3,17 +3,19 @@ import NetworkExtension
 import CocoaLumberjackSwift
 
 class NWTCPSocket : NSObject, RawSocketProtocol {
+    static let ScannerReadTag = 10000
+    
     weak var delegate: RawSocketDelegate?
     
     var connection: NWTCPConnection!
-    //    var lastError: NSError?
-    //    private var dataQueue = Queue<NSData>()
-    //    private var writingData = false
     var delegateQueue: dispatch_queue_t!
     
-    var readPending = false
     var writePending = false
-    var closeAfterReadingAndWriting = false
+    var closeAfterWriting = false
+    
+    var scanner: StreamScanner!
+    var scannerTag: Int!
+    var readDataPrefix: NSData?
     
     var connected: Bool {
         return connection.state == .Connected
@@ -34,7 +36,7 @@ class NWTCPSocket : NSObject, RawSocketProtocol {
     }
     
     func disconnect() {
-        closeAfterReadingAndWriting = true
+        closeAfterWriting = true
         checkStatus()
     }
     
@@ -47,15 +49,33 @@ class NWTCPSocket : NSObject, RawSocketProtocol {
     }
     
     func readDataWithTag(tag: Int) {
-        read(tag)
+        connection.readMinimumLength(0, maximumLength: Opt.MAXNWTCPSocketReadDataSize) { data, error in
+            guard error == nil else {
+                DDLogError("NWTCPSocket got an error when reading data: \(error)")
+                return
+            }
+            
+            self.readCallback(data, tag: tag)
+        }
     }
     
     func readDataToLength(length: Int, withTag tag: Int) {
-        read(tag, length: length)
+        connection.readLength(length) { data, error in
+            guard error == nil else {
+                DDLogError("NWTCPSocket got an error when reading data: \(error)")
+                return
+            }
+            
+            self.readCallback(data, tag: tag)
+        }
     }
     
+    // Actually, this method is available as `- (void)readToPattern:(id)arg1 maximumLength:(unsigned int)arg2 completionHandler:(id /* block */)arg3;` which is sadly not available in public header for some reason I don't know.
+    // I don't want to do it myself since This method is not trival to implement and I don't like reinventing the wheel. Here is only the most naive version, which may not be the optimal if using with large data blocks.
     func readDataToData(data: NSData, withTag tag: Int) {
-        read(tag)
+        scanner = StreamScanner(pattern: data, maximumLength: Opt.MAXNWTCPScanLength)
+        scannerTag = tag
+        readDataWithTag(NWTCPSocket.ScannerReadTag)
     }
     
     private func delegateCall(block: ()->()) {
@@ -102,6 +122,35 @@ class NWTCPSocket : NSObject, RawSocketProtocol {
         }
     }
     
+    private func readCallback(data: NSData?, tag: Int) {
+        guard let data = consumeReadData(data) else {
+            // remote read is closed, but this is okay, nothing need to be done, if this socket is read again, then error occurs.
+            return
+        }
+        
+        if tag == NWTCPSocket.ScannerReadTag {
+            guard let (match, rest) = scanner.addAndScan(data) else {
+                readDataWithTag(NWTCPSocket.ScannerReadTag)
+                return
+            }
+            
+            guard let matchData = match else {
+                // do not find match in the given length, stop now
+                return
+            }
+            
+            readDataPrefix = rest
+            delegateCall() {
+                self.delegate?.didReadData(matchData, withTag: self.scannerTag, from: self)
+            }
+        } else {
+            delegateCall() {
+                self.delegate?.didReadData(data, withTag: tag, from: self)
+            }
+        }
+        
+    }
+    
     private func sendData(data: NSData, withTag tag: Int) {
         writePending = true
         self.connection.write(data) { error in
@@ -120,32 +169,28 @@ class NWTCPSocket : NSObject, RawSocketProtocol {
         }
     }
     
+    private func consumeReadData(data: NSData?) -> NSData? {
+        if readDataPrefix == nil {
+            return data
+        }
+        
+        if data == nil {
+            return readDataPrefix
+        }
+        
+        let wholeData = NSMutableData(data: readDataPrefix!)
+        wholeData.appendData(data!)
+        readDataPrefix = nil
+        scanner = nil
+        return NSData(data: wholeData)
+    }
+    
     private func cancel() {
         connection.cancel()
     }
     
-    private func read(tag: Int, length: Int = 0) {
-        readPending = true
-        connection.readMinimumLength(length, maximumLength: 30000) { data, error in
-            self.readPending = false
-            guard error == nil else {
-                DDLogError("NWTCPSocket got an error when reading data: \(error)")
-                self.disconnect()
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            self.delegateCall() {
-                self.delegate?.didReadData(data, withTag: tag, from: self)
-            }
-        }
-    }
-    
     private func checkStatus() {
-        if closeAfterReadingAndWriting && !readPending && !writePending {
+        if closeAfterWriting && !writePending {
             cancel()
         }
     }
---
+++ NEKit/StreamScanner.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+class StreamScanner {
+    var receivedData: NSMutableData = NSMutableData()
+    let pattern: NSData
+    let maximumLength: Int
+    var finished = false
+    
+    var currentLength: Int {
+        return receivedData.length
+    }
+    
+    init(pattern: NSData, maximumLength: Int) {
+        self.pattern = pattern
+        self.maximumLength = maximumLength
+    }
+    
+    // I know this is not the most effcient algorithm if there is a large number of NSDatas, but since we only need to find the CRLF in http header (as of now), and it should be ready in the first readData call, there is no need to implement a complicate algorithm which is very likely to be slower in such case.
+    func addAndScan(data: NSData) -> (NSData?, NSData)? {
+        guard finished == false else {
+            return nil
+        }
+        
+        receivedData.appendData(data)
+        let startind = max(0, receivedData.length - pattern.length - data.length)
+        let range = receivedData.rangeOfData(pattern, options: .Backwards, range: NSRange(location: startind, length: receivedData.length - startind))
+        
+        if range.location == NSNotFound {
+            if receivedData.length > maximumLength {
+                finished = true
+                return (nil, receivedData)
+            } else {
+                return nil
+            }
+        } else {
+            finished = true
+            let foundEndIndex = range.location + range.length
+            return (receivedData.subdataWithRange(NSRange(location: 0, length: foundEndIndex)), receivedData.subdataWithRange(NSRange(location: foundEndIndex, length: receivedData.length - foundEndIndex)))
+        }
+    }
+}
