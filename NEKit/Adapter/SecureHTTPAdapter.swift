682f7555b07f44b9b84b0b30b7e7b8b4ed419f17
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-14 11:02:50 +0800

    Fix HTTP proxy over SSL

--- NEKit/Adapter/SecureHTTPAdapter.swift
+++ NEKit/Adapter/SecureHTTPAdapter.swift
@@ -3,6 +3,6 @@ import Foundation
 class SecureHTTPAdapter: HTTPAdapter {
     override init(serverHost: String, serverPort: Int, auth: Authentication?) {
         super.init(serverHost: serverHost, serverPort: serverPort, auth: auth)
-        secured = false
+        secured = true
     }
 }
