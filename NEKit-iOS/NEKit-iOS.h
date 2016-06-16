182e234763ca2abd26f29267429d1743acefdb44
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-16 10:41:10 +0800

    Reorganize file

---
+++ NEKit-iOS/NEKit-iOS.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for NEKit-iOS.
+FOUNDATION_EXPORT double NEKit_iOS;
+
+//! Project version string for NEKit_iOS.
+FOUNDATION_EXPORT const unsigned char NEKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NEKit_iOS/PublicHeader.h>
+
+
