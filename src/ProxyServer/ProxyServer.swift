56a82535fafcd7b04f0472d6488957d18a2af06f
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-16 21:41:01 +0800

    Refine doc for ProxyServer

--- src/ProxyServer/ProxyServer.swift
+++ src/ProxyServer/ProxyServer.swift
@@ -13,6 +13,8 @@ public class ProxyServer: NSObject, TunnelDelegate {
     /// The main proxy.
     ///
     /// There can be arbitrary number of proxies running at the same time. However, it is assumed that there is a main proxy server that handles connections that do not target any proxies but still should be proxied.
+    ///
+    /// - warning: This must be set before any connection is created.
     public static var mainProxy: ProxyServer!
 
     /// The port of proxy server.
