33d09232bce589a7473e8adb1a1427afcb3b8c7b
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-19 14:25:51 +0800

    Refactor and add doc for RawSocketFactory

--- src/RawSocket/RawSocketFactory.swift
+++ src/RawSocket/RawSocketFactory.swift
@@ -1,11 +1,36 @@
 import Foundation
 
+/**
+ Represents the type of the socket.
+
+ - NW:  The socket based on `NWTCPConnection`.
+ - GCD: The socket based on `GCDAsyncSocket`.
+ */
+enum SocketBaseType {
+    case NW, GCD
+}
+
+/// Factory to create `RawTCPSocket` based on configuration.
 class RawSocketFactory {
-    static func getRawSocket() -> RawTCPSocketProtocol {
-        if NetworkInterface.TunnelProvider == nil {
-            return GCDTCPSocket()
-        } else {
+    /**
+     Return `RawTCPSocket` instance.
+
+     - parameter type: The type of the socket.
+
+     - returns: The created socket instance.
+     */
+    static func getRawSocket(type: SocketBaseType? = nil) -> RawTCPSocketProtocol {
+        switch type {
+        case .Some(.NW):
             return NWTCPSocket()
+        case .Some(.GCD):
+            return GCDTCPSocket()
+        case nil:
+            if NetworkInterface.TunnelProvider == nil {
+                return GCDTCPSocket()
+            } else {
+                return NWTCPSocket()
+            }
         }
     }
 }
