11353c1b8902b1f992123dd645bbab97e8e8eab6
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-26 15:54:49 +0800

    Fix import error

--- NEKit-macOS/NEKit.h
+++ NEKit-macOS/NEKit.h
@@ -7,5 +7,3 @@ FOUNDATION_EXPORT double NEKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char NEKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NEKit/PublicHeader.h>
-
-#import <NEKit/LibresolvWrapper.h>
