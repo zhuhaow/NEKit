e9cdbe62b7dbb219d909a14d0d8a7aa3d7455f9a
Zhuhao Wang <zhuhaow@gmail.com>
2016-07-06 10:43:43 +0800

    Update demo to the latest API

--- NEKitDemo/AppDelegate.swift
+++ NEKitDemo/AppDelegate.swift
@@ -1,29 +1,27 @@
 import Cocoa
 import NEKit
-import CocoaLumberjackSwift
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-    var proxy: GCDSOCKS5ProxyServer!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        DDLog.addLogger(DDTTYLogger.sharedInstance(), withLevel: .All)
         let config = Configuration()
         let filepath = (NSHomeDirectory() as NSString).stringByAppendingPathComponent(".NEKit_demo.yaml")
         if config.load(fromConfigFile: filepath) {
             RuleManager.currentManager = config.ruleManager
-            proxy = GCDSOCKS5ProxyServer(address: IPv4Address(fromString: "127.0.0.1"), port: Port(port: UInt16(config.proxyPort!)))
+            ProxyServer.mainProxy = GCDSOCKS5ProxyServer(address: IPv4Address(fromString: "127.0.0.1"), port: Port(port: UInt16(config.proxyPort!)))
             // swiftlint:disable force_try
-            try! proxy.start()
+            try! ProxyServer.mainProxy.start()
         } else {
+            NSLog("Failed to load config file.")
             exit(1)
         }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        proxy.stop()
+        ProxyServer.mainProxy.stop()
     }
 
 
