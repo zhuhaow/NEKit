66f5947fa5d2eeccb393dfc92d09bd9f4e87b8aa
Zhuhao Wang <zhuhaow@gmail.com>
2016-08-10 13:35:50 +0800

    Remove demo and unused files

--- Demo/LibresolvWrapper.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-// TODO: Current workaround should be fixed. This class should never be exposed. We have to define a module.
-@interface LibresolvWrapper : NSObject
-+ (nonnull NSArray<NSString *> *) fetchDNSServers;
-@end
