482b7feae304501a8d4ddfab0b799d91a746dcb6
mao <mao@Macintosh.local>
2016-08-31 20:19:43 +0800

    public class DirectAdapterFactory

--- src/Socket/AdapterSocket/Factory/AdapterFactory.swift
+++ src/Socket/AdapterSocket/Factory/AdapterFactory.swift
@@ -28,4 +28,4 @@ public class AdapterFactory {
 /// Factory building direct adapters.
 ///
 /// - note: This is needed since we need to identify direct adapter factory.
-class DirectAdapterFactory: AdapterFactory {}
+public class DirectAdapterFactory: AdapterFactory {}
