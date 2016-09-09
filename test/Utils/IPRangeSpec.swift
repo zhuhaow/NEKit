a5417236e6ca933db49c507dfd83d08ce512ede1
Zhuhao Wang <zhuhaow@gmail.com>
2016-09-09 09:54:58 +0800

    Refine test for IPRange

--- test/Utils/IPRangeSpec.swift
+++ test/Utils/IPRangeSpec.swift
@@ -4,24 +4,80 @@ import Nimble
 
 class IPRangeSpec: QuickSpec {
     override func spec() {
+        let cidrWrongSamples = [
+            ("127.0.0.132", IPRangeError.InvalidCIDRFormat),
+            ("13.1242.1241.1/3", IPRangeError.InvalidCIDRFormat),
+            ("123.122.33.21/35", IPRangeError.InvalidCIDRFormat),
+            ("123.123.131.12/-1", IPRangeError.InvalidCIDRFormat),
+            ("123.123.131.12/", IPRangeError.InvalidCIDRFormat)
+        ]
+
+        let cidrCorrectSamples = [
+            ("127.0.0.0/32", [IPv4Address(fromString: "127.0.0.1")!]),
+            ("127.0.0.0/31", [IPv4Address(fromString: "127.0.0.1")!]),
+            ("127.0.0.0/1", [IPv4Address(fromString: "127.0.0.1")!])
+        ]
+
+        let rangeWrongSamples = [
+            ("127.0.0.132", IPRangeError.InvalidRangeFormat),
+            ("13.1242.1241.1+3", IPRangeError.InvalidRangeFormat),
+            ("255.255.255.255+1", IPRangeError.RangeIsTooLarge),
+            ("0.0.0.1+4294967295", IPRangeError.RangeIsTooLarge),
+            ("123.123.131.12+", IPRangeError.InvalidRangeFormat),
+            ("12.124.51.23-1", IPRangeError.InvalidRangeFormat)
+        ]
+
+        let rangeCorrectSamples = [
+            ("127.0.0.1+3", [IPv4Address(fromString: "127.0.0.1")!]),
+            ("255.255.255.255+0", [IPv4Address(fromString: "255.255.255.255")!]),
+            ("0.0.0.0+4294967295", [IPv4Address(fromString: "0.0.0.0")!])
+        ]
+
+        let ipSamples = [
+            ("127.0.0.1", [IPv4Address(fromString: "127.0.0.1")!])
+        ]
+
         describe("IPRange initailization") {
             it("can be initailized with CIDR IP representation") {
-                // CIDR must have a "/"
-                expect {try IPRange(withCIDRString: "127.0.0.132")}.to(throwError(IPRangeError.InvalidCIDRFormat))
+                for sample in cidrWrongSamples {
+                    expect {try IPRange(withCIDRString: sample.0)}.to(throwError(sample.1))
+                }
 
-                // IP address has to be valid
-                expect {try IPRange(withCIDRString: "13.1242.1241.1/3")}.to(throwError(IPRangeError.InvalidCIDRFormat))
+                for sample in cidrCorrectSamples {
+                    expect {try IPRange(withCIDRString: sample.0)}.toNot(throwError())
+                }
+            }
 
-                // mask has to be valid
-                expect {try IPRange(withCIDRString: "123.122.33.21/35")}.to(throwError(IPRangeError.InvalidCIDRFormat))
-                expect {try IPRange(withCIDRString: "123.123.131.12/-1")}.to(throwError(IPRangeError.InvalidCIDRFormat))
+            it("can be initailized with IP range representation") {
+                for sample in rangeWrongSamples {
+                    expect {try IPRange(withRangeString: sample.0)}.to(throwError(sample.1))
+                }
 
-                expect {try IPRange(withCIDRString: "127.0.0.0/31")}.toNot(throwError())
+                for sample in rangeCorrectSamples {
+                    expect {try IPRange(withRangeString: sample.0)}.toNot(throwError())
+                }
+            }
 
-                let range = try! IPRange(withCIDRString: "127.0.0.0/31")
-                
-                expect(range.baseIP.presentation) == "127.0.0.0"
-                expect(range.range) == 2 - 1
+            it("can select the best way to initailize") {
+                for sample in cidrWrongSamples {
+                    expect {try IPRange(withString: sample.0)}.to(throwError(sample.1))
+                }
+
+                for sample in rangeWrongSamples {
+                    expect {try IPRange(withString: sample.0)}.toNot(throwError(sample.1))
+                }
+
+                for sample in cidrCorrectSamples {
+                    expect {try IPRange(withString: sample.0)}.toNot(throwError())
+                }
+
+                for sample in rangeCorrectSamples {
+                    expect {try IPRange(withString: sample.0)}.toNot(throwError())
+                }
+
+                for sample in ipSamples {
+                    expect {try IPRange(withString: sample.0)}.toNot(throwError())
+                }
             }
         }
     }
