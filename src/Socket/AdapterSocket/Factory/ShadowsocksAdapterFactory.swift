dffd2dd5d6e8c0951759f8164c18944665c3469c
Zhuhao Wang <zhuhaow@gmail.com>
2016-11-19 14:56:56 +0800

    Fix ShadowsocksAdapterFactory

--- src/Socket/AdapterSocket/Factory/ShadowsocksAdapterFactory.swift
+++ src/Socket/AdapterSocket/Factory/ShadowsocksAdapterFactory.swift
@@ -4,19 +4,21 @@ import Foundation
 open class ShadowsocksAdapterFactory: ServerAdapterFactory {
     let encryptAlgorithm: CryptoAlgorithm
     let password: String
+    let streamObfuscaterType: ShadowsocksStreamObfuscater.Type
 
-    public init(serverHost: String, serverPort: Int, encryptAlgorithm: CryptoAlgorithm, password: String) {
+    public init(serverHost: String, serverPort: Int, encryptAlgorithm: CryptoAlgorithm, password: String, streamObfuscaterType: ShadowsocksStreamObfuscater.Type = ShadowsocksAdapter.OriginStreamObfuscater.self) {
         self.encryptAlgorithm = encryptAlgorithm
         self.password = password
+        self.streamObfuscaterType = streamObfuscaterType
         super.init(serverHost: serverHost, serverPort: serverPort)
     }
 
-    public convenience init?(serverHost: String, serverPort: Int, encryptAlgorithm: String, password: String) {
+    public convenience init?(serverHost: String, serverPort: Int, encryptAlgorithm: String, password: String, streamObfuscaterType: ShadowsocksStreamObfuscater.Type = ShadowsocksAdapter.OriginStreamObfuscater.self) {
         guard let encryptAlgorithm = CryptoAlgorithm(rawValue: encryptAlgorithm.uppercased()) else {
             return nil
         }
 
-        self.init(serverHost: serverHost, serverPort: serverPort, encryptAlgorithm: encryptAlgorithm, password: password)
+        self.init(serverHost: serverHost, serverPort: serverPort, encryptAlgorithm: encryptAlgorithm, password: password, streamObfuscaterType: streamObfuscaterType)
     }
 
     /**
@@ -27,7 +29,7 @@ open class ShadowsocksAdapterFactory: ServerAdapterFactory {
      - returns: The built adapter.
      */
     override func getAdapter(_ request: ConnectRequest) -> AdapterSocket {
-        let adapter = ShadowsocksAdapter(host: serverHost, port: serverPort, encryptAlgorithm: encryptAlgorithm, password: password)
+        let adapter = ShadowsocksAdapter(host: serverHost, port: serverPort, encryptAlgorithm: encryptAlgorithm, password: password, streamObfuscaterType: streamObfuscaterType)
         adapter.socket = RawSocketFactory.getRawSocket()
         return adapter
     }
