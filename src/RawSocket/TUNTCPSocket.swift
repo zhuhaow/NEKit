52e61d0234ecd1e1894eaeb1b906447713002793
Zhuhao Wang <zhuhaow@gmail.com>
2016-12-04 22:52:51 +0800

    Update `TUNTCPSocket`

--- src/RawSocket/TUNTCPSocket.swift
+++ src/RawSocket/TUNTCPSocket.swift
@@ -4,16 +4,15 @@ import tun2socks
 /// The TCP socket build upon `TSTCPSocket`.
 ///
 /// - warning: This class is not thread-safe, it is expected that the instance is accessed on the `queue` only.
-open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
+public class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
     fileprivate let tsSocket: TSTCPSocket
-    fileprivate var readTag: Int?
+    fileprivate var reading = false
     fileprivate var pendingReadData: Data = Data()
-    fileprivate var writeTag: Int!
     fileprivate var remainWriteLength: Int = 0
     fileprivate var closeAfterWriting = false
 
     fileprivate var scanner: StreamScanner?
-
+    
     fileprivate var readLength: Int?
 
     /**
@@ -29,51 +28,56 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
     // MARK: RawTCPSocketProtocol implementation
 
     /// The `RawTCPSocketDelegate` instance.
-    open weak var delegate: RawTCPSocketDelegate?
+    public weak var delegate: RawTCPSocketDelegate?
 
     /// Every method call and variable access must operated on this queue. And all delegate methods will be called on this queue.
     ///
     /// - warning: This should be set as soon as the instance is initialized.
-    open var queue: DispatchQueue!
+    public var queue: DispatchQueue!
 
     /// If the socket is connected.
-    open var isConnected: Bool {
+    public var isConnected: Bool {
         return tsSocket.isConnected
     }
 
     /// The source address.
-    open var sourceIPAddress: IPv4Address? {
+    public var sourceIPAddress: IPv4Address? {
         return IPv4Address(fromInAddr: tsSocket.sourceAddress)
     }
 
     /// The source port.
-    open var sourcePort: Port? {
+    public var sourcePort: Port? {
         return Port(port: tsSocket.sourcePort)
     }
 
     /// The destination address.
-    open var destinationIPAddress: IPv4Address? {
+    public var destinationIPAddress: IPv4Address? {
         return IPv4Address(fromInAddr: tsSocket.destinationAddress)
     }
 
     /// The destination port.
-    open var destinationPort: Port? {
+    public var destinationPort: Port? {
         return Port(port: tsSocket.destinationPort)
     }
-
-    /**
-     `TUNTCPSocket` cannot connect to anything actively, this is just a stub method.
-     */
-    open func connectTo(_ host: String, port: Int, enableTLS: Bool, tlsSettings: [AnyHashable: Any]?) throws {}
+    
+    /// `TUNTCPSocket` cannot connect to anything actively, this is just a stub method.
+    ///
+    /// - Parameters:
+    ///   - host: host
+    ///   - port: port
+    ///   - enableTLS: enableTLS
+    ///   - tlsSettings: tlsSettings
+    /// - Throws: Never throws anything.
+    public func connectTo(host: String, port: Int, enableTLS: Bool, tlsSettings: [AnyHashable: Any]?) throws {}
 
     /**
      Disconnect the socket.
 
      The socket will disconnect elegantly after any queued writing data are successfully sent.
      */
-    open func disconnect() {
+    public func disconnect() {
         if !isConnected {
-            delegate?.didDisconnect(self)
+            delegate?.didDisconnectWith(socket: self)
         } else {
             closeAfterWriting = true
             checkStatus()
@@ -83,9 +87,9 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
     /**
      Disconnect the socket immediately.
      */
-    open func forceDisconnect() {
+    public func forceDisconnect() {
         if !isConnected {
-            delegate?.didDisconnect(self)
+            delegate?.didDisconnectWith(socket: self)
         } else {
             tsSocket.close()
         }
@@ -95,11 +99,9 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
      Send data to local.
 
      - parameter data: Data to send.
-     - parameter tag:  The tag identifying the data in the callback delegate method.
      - warning: This should only be called after the last write is finished, i.e., `delegate?.didWriteData()` is called.
      */
-    open func writeData(_ data: Data, withTag tag: Int) {
-        writeTag = tag
+    public func write(data: Data) {
         remainWriteLength = data.count
         tsSocket.writeData(data)
     }
@@ -107,11 +109,10 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
     /**
      Read data from the socket.
 
-     - parameter tag: The tag identifying the data in the callback delegate method.
      - warning: This should only be called after the last read is finished, i.e., `delegate?.didReadData()` is called.
      */
-    open func readDataWithTag(_ tag: Int) {
-        readTag = tag
+    public func readData() {
+        reading = true
         checkReadData()
     }
 
@@ -119,38 +120,35 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
      Read specific length of data from the socket.
 
      - parameter length: The length of the data to read.
-     - parameter tag:    The tag identifying the data in the callback delegate method.
      - warning: This should only be called after the last read is finished, i.e., `delegate?.didReadData()` is called.
      */
-    open func readDataToLength(_ length: Int, withTag tag: Int) {
+    public func readDataTo(length: Int) {
         readLength = length
-        readTag = tag
-        checkStatus()
+        reading = true
+        checkReadData()
     }
 
     /**
      Read data until a specific pattern (including the pattern).
 
      - parameter data: The pattern.
-     - parameter tag:  The tag identifying the data in the callback delegate method.
      - warning: This should only be called after the last read is finished, i.e., `delegate?.didReadData()` is called.
      */
-    open func readDataToData(_ data: Data, withTag tag: Int) {
-        readDataToData(data, withTag: tag, maxLength: 0)
+    public func readDataTo(data: Data) {
+        readDataTo(data: data, maxLength: 0)
     }
 
     /**
      Read data until a specific pattern (including the pattern).
 
      - parameter data: The pattern.
-     - parameter tag:  The tag identifying the data in the callback delegate method.
      - parameter maxLength: Ignored since `GCDAsyncSocket` does not support this. The max length of data to scan for the pattern.
      - warning: This should only be called after the last read is finished, i.e., `delegate?.didReadData()` is called.
      */
-    open func readDataToData(_ data: Data, withTag tag: Int, maxLength: Int) {
-        readTag = tag
+    public func readDataTo(data: Data, maxLength: Int) {
+        reading = true
         scanner = StreamScanner(pattern: data, maximumLength: maxLength)
-        checkStatus()
+        checkReadData()
     }
 
     fileprivate func queueCall(_ block: @escaping ()->()) {
@@ -160,8 +158,7 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
     fileprivate func checkReadData() {
         if pendingReadData.count > 0 {
             queueCall {
-                // the didReadData might change the `readTag`
-                guard let tag = self.readTag else {
+                guard self.reading else {
                     // no queued read request
                     return
                 }
@@ -172,8 +169,8 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
                         self.pendingReadData = self.pendingReadData.subdata(in: readLength..<self.pendingReadData.count)
 
                         self.readLength = nil
-                        self.delegate?.didReadData(returnData, withTag: tag, from: self)
-                        self.readTag = nil
+                        self.delegate?.didRead(data: returnData, from: self)
+                        self.reading = false
                     }
                 } else if let scanner = self.scanner {
                     guard let (match, rest) = scanner.addAndScan(self.pendingReadData) else {
@@ -181,7 +178,6 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
                     }
 
                     self.scanner = nil
-                    self.readTag = nil
 
                     guard let matchData = match else {
                         // do not find match in the given length, stop now
@@ -189,11 +185,12 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
                     }
 
                     self.pendingReadData = rest
-                    self.delegate?.didReadData(matchData, withTag: tag, from: self)
+                    self.delegate?.didRead(data: matchData, from: self)
+                    self.reading = false
                 } else {
-                    self.readTag = nil
-                    self.delegate?.didReadData(self.pendingReadData, withTag: tag, from: self)
+                    self.delegate?.didRead(data: self.pendingReadData, from: self)
                     self.pendingReadData = Data()
+                    self.reading = false
                 }
             }
         }
@@ -223,7 +220,7 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
 
     open func socketDidClose(_ socket: TSTCPSocket) {
         queueCall {
-            self.delegate?.didDisconnect(self)
+            self.delegate?.didDisconnectWith(socket: self)
             self.delegate = nil
         }
     }
@@ -240,7 +237,7 @@ open class TUNTCPSocket: RawTCPSocketProtocol, TSTCPSocketDelegate {
             self.remainWriteLength -= length
             if self.remainWriteLength <= 0 {
 
-                self.delegate?.didWriteData(nil, withTag: self.writeTag, from: self)
+                self.delegate?.didWrite(data: nil, by: self)
                 self.checkStatus()
             }
         }
