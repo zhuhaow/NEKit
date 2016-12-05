afc6bed2a5b965811b5f29b20c9af4112f3172c9
Zhuhao Wang <zhuhaow@gmail.com>
2016-12-05 11:18:58 +0800

    Update `DirectProxySocket`

--- src/Socket/ProxySocket/DirectProxySocket.swift
+++ src/Socket/ProxySocket/DirectProxySocket.swift
@@ -1,58 +1,57 @@
 import Foundation
 
-/// This class just forwards data directly. It is designed to work with tun2socks.
-open class DirectProxySocket: ProxySocket {
+/// This class just forwards data directly. 
+/// - note: It is designed to work with tun2socks only.
+public class DirectProxySocket: ProxySocket {
     /**
      Begin reading and processing data from the socket.
 
      - note: Since there is nothing to read and process before forwarding data, this just calls `delegate?.didReceiveRequest`.
      */
-    override func openSocket() {
+    override public func openSocket() {
         super.openSocket()
 
         if let address = socket.destinationIPAddress, let port = socket.destinationPort {
             request = ConnectRequest(host: address.presentation, port: Int(port.value))
 
             observer?.signal(.receivedRequest(request!, on: self))
-            delegate?.didReceiveRequest(request!, from: self)
+            delegate?.didReceive(request: request!, from: self)
         } else {
             forceDisconnect()
         }
     }
 
     /**
-     Response to the `ConnectResponse` from `AdapterSocket` on the other side of the `Tunnel`.
-
-     - parameter response: The response is ignored.
+     Response to the `AdapterSocket` on the other side of the `Tunnel` which has succefully connected to the remote server.
+     
+     - parameter adapter: The `AdapterSocket`.
      */
-    override func respondToResponse(_ response: ConnectResponse) {
-        super.respondToResponse(response)
+    override public func respondTo(adapter: AdapterSocket) {
+        super.respondTo(adapter: adapter)
 
-        observer?.signal(ProxySocketEvent.readyForForward(self))
-        delegate?.readyToForward(self)
+        observer?.signal(.readyForForward(self))
+        delegate?.didBecomeReadyToForwardWith(socket: self)
     }
 
     /**
      The socket did read some data.
 
      - parameter data:    The data read from the socket.
-     - parameter withTag: The tag given when calling the `readData` method.
      - parameter from:    The socket where the data is read from.
      */
-    override open func didReadData(_ data: Data, withTag tag: Int, from: RawTCPSocketProtocol) {
-        super.didReadData(data, withTag: tag, from: from)
-        delegate?.didReadData(data, withTag: tag, from: self)
+    override open func didRead(data: Data, from: RawTCPSocketProtocol) {
+        super.didRead(data: data, from: from)
+        delegate?.didRead(data: data, from: self)
     }
 
     /**
      The socket did send some data.
 
      - parameter data:    The data which have been sent to remote (acknowledged). Note this may not be available since the data may be released to save memory.
-     - parameter withTag: The tag given when calling the `writeData` method.
-     - parameter from:    The socket where the data is sent out.
+     - parameter by:    The socket where the data is sent out.
      */
-    override open func didWriteData(_ data: Data?, withTag tag: Int, from: RawTCPSocketProtocol) {
-        super.didWriteData(data, withTag: tag, from: from)
-        delegate?.didWriteData(data, withTag: tag, from: self)
+    override open func didWrite(data: Data?, by: RawTCPSocketProtocol) {
+        super.didWrite(data: data, by: from)
+        delegate?.didWrite(data: data, by: self)
     }
 }
