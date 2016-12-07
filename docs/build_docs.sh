f07fb053a93386e2698ce41f0b6d7317b32bd580
Zhuhao Wang <zhuhaow@gmail.com>
2016-12-07 12:46:18 +0800

    Setting up Travis CI

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,26 +1,49 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+# Automated deploy script with Travis CI.
+
+# Exit if any subcommand fails.
+set -e
 
 # only build doc for master branch
 export SOURCE_BRANCH="master"
-export DOC_BRANCH="gh-pages"
 
-if [ "$CI_PULL_REQUEST" != "" -o "$CIRCLE_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Only build for master branch"
     exit 0
 fi
 
+# Variables
+ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+
+echo "Started deploying"
+
+# Checkout gh-pages branch.
+if [ `git branch | grep gh-pages` ]
+then
+    git branch -D gh-pages
+fi
+git checkout -b gh-pages
 
 # build docs now
 bundle update
 bundle exec jazzy --config docs/.jazzy.yaml
 
-# remove all redundant files
-find . -not -name "docs" -not -name ".git" -maxdepth 1 -print0 | xargs -0 rm -rf --
-# copy docs files to root
-cp -r docs/. .
-rm -rf docs
+# Delete and move files.
+find . -maxdepth 1 ! -name 'docs' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv docs/* .
+rm -R docs/
 
-# push update
-git checkout --orphan gh-pages
-git add .
-git -c user.name="Circle CI" commit -m "Update docs [skip ci]"
-git push --force --quiet https://$GITHUB_TOKEN@github.com/zhuhaow/NEKit.git gh-pages > /dev/null 2>&1
+# Push to gh-pages.
+git config user.name "$USERNAME"
+git config user.email "$EMAIL"
+
+git add -fA
+git commit --allow-empty -m "$COMMIT_MESSAGE [ci skip]"
+git push -f -q $ORIGIN_CREDENTIALS gh-pages
+
+echo "Deployed Successfully!"
+
+exit 0
