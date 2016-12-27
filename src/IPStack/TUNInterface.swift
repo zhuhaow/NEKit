ff69e09eb5d96f1e65c403863c9415348b9d91c8
Zhuhao Wang <zhuhaow@gmail.com>
2016-12-27 16:51:04 +0800

    Fix thread issue for `TUNInterface`

--- src/IPStack/TUNInterface.swift
+++ src/IPStack/TUNInterface.swift
@@ -5,62 +5,71 @@ import NetworkExtension
 open class TUNInterface {
     fileprivate weak var packetFlow: NEPacketTunnelFlow?
     fileprivate var stacks: [IPStackProtocol] = []
-
+    
     /**
      Initialize TUN interface with a packet flow.
-
+     
      - parameter packetFlow: The packet flow to work with.
      */
     public init(packetFlow: NEPacketTunnelFlow) {
         self.packetFlow = packetFlow
     }
-
+    
     /**
      Start processing packets, this should be called after registering all IP stacks.
-
+     
      A stopped interface should never start again. Create a new interface instead.
      */
     open func start() {
-        readPackets()
+        QueueFactory.executeOnQueueSynchronizedly {
+            self.readPackets()
+        }
     }
-
+    
     /**
      Stop processing packets, this should be called before releasing the interface.
      */
     open func stop() {
-        packetFlow = nil
-
-        for stack in stacks {
-            stack.stop()
+        QueueFactory.executeOnQueueSynchronizedly {
+            self.packetFlow = nil
+            
+            for stack in self.stacks {
+                stack.stop()
+            }
+            self.stacks = []
         }
-        stacks = []
     }
-
+    
     /**
      Register a new IP stack.
-
+     
      When a packet is read from TUN interface (the packet flow), it is passed into each IP stack according to the registration order until one of them takes it in.
-
+     
      - parameter stack: The IP stack to append to the stack list.
      */
-    open func registerStack(_ stack: IPStackProtocol) {
-        stack.outputFunc = generateOutputBlock()
-        stacks.append(stack)
+    open func register(stack: IPStackProtocol) {
+        QueueFactory.executeOnQueueSynchronizedly {
+            stack.outputFunc = self.generateOutputBlock()
+            self.stacks.append(stack)
+        }
     }
-
+    
     fileprivate func readPackets() {
         packetFlow?.readPackets { packets, versions in
-            for (i, packet) in packets.enumerated() {
-                for stack in self.stacks {
-                    if stack.inputPacket(packet, version: versions[i]) {
-                        break
+            QueueFactory.getQueue().async {
+                for (i, packet) in packets.enumerated() {
+                    for stack in self.stacks {
+                        if stack.inputPacket(packet, version: versions[i]) {
+                            break
+                        }
                     }
                 }
             }
+            
             self.readPackets()
         }
     }
-
+    
     fileprivate func generateOutputBlock() -> ([Data], [NSNumber]) -> Void {
         return { [weak self] packets, versions in
             self?.packetFlow?.writePackets(packets, withProtocols: versions)
