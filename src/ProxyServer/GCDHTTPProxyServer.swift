491df8455e355a61c7835515cdc91f2d3b653dfd
Zhuhao Wang <zhuhaow@gmail.com>
2018-03-31 15:38:22 +0800

    Fix wrong control level in subclasses

--- src/ProxyServer/GCDHTTPProxyServer.swift
+++ src/ProxyServer/GCDHTTPProxyServer.swift
@@ -17,7 +17,7 @@ public final class GCDHTTPProxyServer: GCDProxyServer {
 
      - parameter socket: The accepted socket.
      */
-    override func handleNewGCDSocket(_ socket: GCDTCPSocket) {
+    override open func handleNewGCDSocket(_ socket: GCDTCPSocket) {
         let proxySocket = HTTPProxySocket(socket: socket)
         didAcceptNewSocket(proxySocket)
     }
